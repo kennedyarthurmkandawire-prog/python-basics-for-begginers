e1b6ad43e8e30e765ec7c73eead7f75c6c61c58d
KENNEDY <kennedyarthurmkandawire@gmail.com>
2026-02-18 09:28:25 -0800

    Add PyCharm project files and initial home.py
    Add IDE configuration for a Python project (.idea/*) including inspection profile, module settings, VCS mappings and an IDE-specific .gitignore. Also add home.py — a small interactive script demonstrating basic prints and input handling (name/age) and simple arithmetic output. These changes set up project metadata and include an initial example script.

---
+++ home.py
@@ -0,0 +1,16 @@
+a,b,c = "kennedy", "john", "mary"
+print(b,c)
+print(a+b ,end="#")
+print(f"{a},{b},{c}")
+
+name = input("Enter your name: ")
+print("where are you from?",name + ",Mkandawire")
+
+age = int(input("Enter your age:"))
+print(f"your age is {age+ 5 }" )
+
+name1 =  input("Enter your name: ")
+age1 = int(input("Enter your age: "))
+
+print(f"hello,{name1}")
+print(f"next year you will  be {age1 +1}")
